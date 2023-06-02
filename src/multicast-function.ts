ebdf5e0d0140beb698a8b945ad9ad8b1ffcda397
Ryuu <2357622935@qq.com>
2023-06-02 14:27:58 +0800

    version 0.2.0 -> 0.3.0 BREAKING CHANGE:MulticastFunction remove size()add length read only property add boolean return value for add(func: T) change remove() return type, T | null -> boolean add null and undefined check in add() add null and undefined check in remove()

--- src/multicast-function.ts
+++ src/multicast-function.ts
@@ -1,32 +1,46 @@
 export class MulticastFunction<T extends (...args: any[]) => any> {
     private readonly functions: T[] = [];
 
-    add(method: T) {
-        this.functions.push(method);
+    get length() {
+        return this.functions.length;
+    }
+
+    add(func: T): boolean {
+        if (func === null || func === undefined) {
+            return false;
+        }
+
+        const length = this.functions.length;
+        const newLength = this.functions.push(func);
+        return newLength > length;
     }
 
     invoke(...args: Parameters<T>): ReturnType<T> {
         let returnValue: ReturnType<T>;
         this.functions.forEach(
-            delegate => returnValue = delegate(...args)
+            (func: T) => returnValue = func(...args)
         );
         return returnValue!;
     }
 
-    remove(methodToBeRemove: T): T | null {
-        return MulticastFunction.removeLastMatchedElement(this.functions, method => method === methodToBeRemove);
+    remove(funcToBeRemove: T): boolean {
+        if (funcToBeRemove === null || funcToBeRemove === undefined) {
+            return false;
+        }
+
+        return MulticastFunction.removeLastMatchedElement(
+            this.functions,
+            func => func === funcToBeRemove
+        );
     }
 
-    size(): number {
-        return this.functions.length;
-    }
-
-    private static removeLastMatchedElement<T>(array: T[], removeIf: (element: T) => boolean): T | null {
+    private static removeLastMatchedElement<T>(array: T[], removeIf: (element: T) => boolean): boolean {
         for (let index = array.length - 1; index >= 0; index--) {
             if (removeIf(array[index])) {
-                return array.splice(index, 1)[0];
+                array.splice(index, 1);
+                return true;
             }
         }
-        return null;
+        return false;
     }
 }
--- tests/multicast-function.test.ts
+++ tests/multicast-function.test.ts
@@ -2,77 +2,119 @@ import {MulticastFunction} from '../src';
 
 describe('MulticastFunction', () => {
     it('add', () => {
-        let multicastFunction = new MulticastFunction<() => void>();
-
-        let delegate = () => {
+        const delegate = () => {
         };
 
+        const multicastFunction = new MulticastFunction<() => void>();
         multicastFunction.add(delegate);
-        expect(multicastFunction.size()).toBe(1);
+        expect(multicastFunction.length).toBe(1);
+    });
+    it('add null', () => {
+        const multicastFunction = new MulticastFunction<() => void>();
+        const isAdd = multicastFunction.add(null);
+        expect(isAdd).toBe(false);
+        expect(multicastFunction.length).toBe(0);
+    });
+    it('add undefined', () => {
+        const multicastFunction = new MulticastFunction<() => void>();
+        const isAdd = multicastFunction.add(undefined);
+        expect(isAdd).toBe(false);
+        expect(multicastFunction.length).toBe(0);
+    });
+    it('add return value', () => {
+        const delegate = () => {
+        };
+        const multicastFunction = new MulticastFunction<() => void>();
+        const isDelegateAdd = multicastFunction.add(delegate);
+        expect(isDelegateAdd).toBe(true);
+
+        const isNullAdd = multicastFunction.add(null);
+        expect(isNullAdd).toBe(false);
     });
     it('repeat add', () => {
-        let multicastFunction = new MulticastFunction<() => void>();
-
-        let delegate = () => {
+        const delegate = () => {
         };
-
+        const multicastFunction = new MulticastFunction<() => void>();
         multicastFunction.add(delegate);
         multicastFunction.add(delegate);
-        expect(multicastFunction.size()).toBe(2);
+        expect(multicastFunction.length).toBe(2);
     });
     it('invoke', () => {
-        let multicastFunction = new MulticastFunction<() => void>();
-
         let value = 0;
 
-        let delegate = () => {
+        const delegate = () => {
             value++;
         };
 
+        const multicastFunction = new MulticastFunction<() => void>();
         multicastFunction.add(delegate);
         multicastFunction.invoke();
         expect(value).toBe(1);
     });
     it('remove', () => {
-        let multicastFunction = new MulticastFunction<() => void>();
-
-        let delegate = () => {
+        const delegate = () => {
         };
 
+        const multicastFunction = new MulticastFunction<() => void>();
         multicastFunction.add(delegate);
         multicastFunction.remove(delegate);
-        expect(multicastFunction.size()).toBe(0);
+        expect(multicastFunction.length).toBe(0);
+    });
+    it('remove not exist value', () => {
+        const delegate = () => {
+        };
+
+        const delegateNotInMulticast = () => {
+        };
+
+        const multicastFunction = new MulticastFunction<() => void>();
+        multicastFunction.add(delegate);
+        const isRemove = multicastFunction.remove(delegateNotInMulticast);
+        expect(isRemove).toBe(false);
+        expect(multicastFunction.length).toBe(1);
+    });
+    it('remove return value', () => {
+        const delegateToBeRemove = () => {
+        };
+        const multicastFunction = new MulticastFunction<() => void>();
+        multicastFunction.add(delegateToBeRemove);
+        const isDelegateToBeRemoveRemoved = multicastFunction.remove(delegateToBeRemove);
+        expect(isDelegateToBeRemoveRemoved).toBe(true);
+
+        const delegateNotInMulticast = () => {
+        };
+        const isDelegateNotInMulticastRemoved = multicastFunction.remove(delegateNotInMulticast);
+        expect(isDelegateNotInMulticastRemoved).toBe(false);
     });
     it('remove duplicate', () => {
-        let multicastFunction = new MulticastFunction<() => void>();
-
-        let delegate = () => {
+        const delegate = () => {
         };
 
+        const multicastFunction = new MulticastFunction<() => void>();
         multicastFunction.add(delegate);
         multicastFunction.add(delegate);
         multicastFunction.remove(delegate);
-        expect(multicastFunction.size()).toBe(1);
+        expect(multicastFunction.length).toBe(1);
     });
     it('remove null', () => {
-        let multicastFunction = new MulticastFunction<() => void>();
-
-        let delegate = () => {
-        };
-
-        multicastFunction.add(delegate);
-        // @ts-ignore
-        multicastFunction.remove(null);
-        expect(multicastFunction.size()).toBe(1);
+        const multicastFunction = new MulticastFunction<() => void>();
+        const isRemove = multicastFunction.remove(null);
+        expect(isRemove).toBe(false);
+        expect(multicastFunction.length).toBe(0);
     });
-    it('size', () => {
-        let multicastFunction = new MulticastFunction<() => void>();
-
-        let delegate = () => {
+    it('remove undefined', () => {
+        const multicastFunction = new MulticastFunction<() => void>();
+        const isRemove = multicastFunction.remove(undefined);
+        expect(isRemove).toBe(false);
+        expect(multicastFunction.length).toBe(0);
+    });
+    it('length', () => {
+        const delegate = () => {
         };
 
+        const multicastFunction = new MulticastFunction<() => void>();
         multicastFunction.add(delegate);
         multicastFunction.add(delegate);
-        expect(multicastFunction.size()).toBe(2);
+        expect(multicastFunction.length).toBe(2);
     });
 });
