81c0a472eaab7b84fba282a10b262627c078d8fe
Ryuu <2357622935@qq.com>
2023-06-05 15:00:17 +0800

    fix(deps): revert the 'include': 'tests/**/*.ts' in tsconfig.json, otherwise the unit test code will be compiled into the dist folder. fix(deps): remove exclude in tsconfig.json

--- tests/multicast-function.test.ts
+++ tests/multicast-function.test.ts
@@ -10,14 +10,12 @@ describe('MulticastFunction', () => {
     });
     it('add null', () => {
         const multicastFunction = new MulticastFunction<() => void>();
-        // @ts-ignore
         const isAdd = multicastFunction.add(null);
         expect(isAdd).toBe(false);
         expect(multicastFunction.length).toBe(0);
     });
     it('add undefined', () => {
         const multicastFunction = new MulticastFunction<() => void>();
-        // @ts-ignore
         const isAdd = multicastFunction.add(undefined);
         expect(isAdd).toBe(false);
         expect(multicastFunction.length).toBe(0);
@@ -29,7 +27,6 @@ describe('MulticastFunction', () => {
         const isFuncAdd = multicastFunction.add(func);
         expect(isFuncAdd).toBe(true);
 
-        // @ts-ignore
         const isNullAdd = multicastFunction.add(null);
         expect(isNullAdd).toBe(false);
     });
@@ -115,7 +112,6 @@ describe('MulticastFunction', () => {
         };
         multicastFunction.add(func);
 
-        // @ts-ignore
         const isEquals = multicastFunction.equals(null);
 
         expect(isEquals).toBe(false);
@@ -126,7 +122,6 @@ describe('MulticastFunction', () => {
         };
         multicastFunction.add(func);
 
-        // @ts-ignore
         const isEquals = multicastFunction.equals(undefined);
 
         expect(isEquals).toBe(false);
@@ -215,14 +210,12 @@ describe('MulticastFunction', () => {
     });
     it('remove null', () => {
         const multicastFunction = new MulticastFunction<() => void>();
-        // @ts-ignore
         const isRemove = multicastFunction.remove(null);
         expect(isRemove).toBe(false);
         expect(multicastFunction.length).toBe(0);
     });
     it('remove undefined', () => {
         const multicastFunction = new MulticastFunction<() => void>();
-        // @ts-ignore
         const isRemove = multicastFunction.remove(undefined);
         expect(isRemove).toBe(false);
         expect(multicastFunction.length).toBe(0);
